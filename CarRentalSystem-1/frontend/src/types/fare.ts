7db40704f59e9fd4e4feb124bbd606842e5db86c
23WH1A6646 <147157405+23WH1A6646@users.noreply.github.com>
2025-07-21 21:56:06 +0530

    Delete CarRentalSystem-1/frontend directory

--- CarRentalSystem-1/frontend/src/types/fare.ts
+++
@@ -1,7 +0,0 @@
-// src/types/Fare.ts
-export interface FareRequest {
-  carId: number;
-  startDate: string; // ISO format: YYYY-MM-DD
-  endDate: string;
-  totalKmDriven?: number; // optional for now
-}
